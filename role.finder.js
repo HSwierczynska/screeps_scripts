fc98babbf4b4b3aa4d87cd162d99be06cf92e783
Hswierczynska <haniasa96@gmail.com>
2017-07-18 14:27:03 +0200

    Carry found energy to the container

--- role.finder.js
+++ role.finder.js
@@ -9,8 +9,13 @@ var roleFinder = {
         }
         
         else {
+            var containerTarget = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: structure => {
+                    return structure.structureType == STRUCTURE_CONTAINER
+                }
+            });
             if(creep.transfer(STRUCTURE_CONTAINER, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                creep.moveTo(STRUCTURE_CONTAINER);
+                creep.moveTo(containerTarget);
             }
             
         }
