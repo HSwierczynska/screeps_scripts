1d52bba18b6fb8b7d05c42125aef90e000bf3b4e
Hswierczynska <haniasa96@gmail.com>
2017-07-23 13:34:22 +0200

    Builder builds and mantains the road

--- role.builder.js
+++ role.builder.js
@@ -1,7 +1,11 @@
+const actions = require('creeps.actions');
+
 var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+
+        //Building the roads
 
 	      if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
@@ -21,11 +25,36 @@ var roleBuilder = {
             }
 	      }
 	      else {
-	          var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+	          actions.searchingForSources(creep);
             }
 	      }
+
+        // Reapiring the roads
+
+        if(creep.memory.repairing && creep.carry.energy == 0){
+            creep.memory.repairing = false;
+        }
+
+        if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.repairing = true;
+        }
+
+
+        if(creep.memory.repairing) {
+            
+            var closestDamagedWall = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => STRUCTURE_ROAD && structure.hits < (0.75 *structure.hitsMax)
+            });
+            if(closestDamagedWall){
+                if(creep.repair(closestDamagedWall) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(closestDamagedWall,
+                                 {visualizePathstyle: {stroke: '#ffffff'}})};
+            }
+        }
+        else{
+            actions.searchingForSources(creep);
+        }
+
 	  }
 };
 
