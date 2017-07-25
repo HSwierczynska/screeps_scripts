c0b707fed23ae402b592c7d019525ebc9aa4bb02
Hswierczynska <haniasa96@gmail.com>
2017-07-25 09:14:14 +0200

    New role

---
+++ role.spawncourier.js
@@ -0,0 +1,15 @@
+const actions = require('creeps.actions');
+
+var roleCourier = {
+
+    run: function(creep){
+        if(creep.carry.energy< creep.carryCapacity){
+            actions.withdrawFromContainer(creep);
+        }
+        else{
+            actions.transferingToSpawn(creep);
+        }
+    }
+};
+
+module.exports= roleCourier;
