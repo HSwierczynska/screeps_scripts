2e603664244882dc2043516c56818f8df247bd79
Hswierczynska <haniasa96@gmail.com>
2017-07-27 01:23:31 +0200

    One courier type

--- role.courier.js
+++ role.courier.js
@@ -3,11 +3,14 @@ const actions = require('creeps.actions');
 var roleCourier = {
 
     run: function(creep){
-        if(creep.carry.energy< creep.carryCapacity){
+        if(creep.carry.energy == 0){
             actions.withdrawFromContainer(creep);
         }
         else{
-            actions.transferingToExtension(creep);
+            actions.transferingToSpawn(creep);
+            if(Game.spawns.Spawn1.energy == Game.spawns.Spawn1.energyCapacity){
+                actions.transferingToExtension(creep);
+            }
         }
     }
 };
