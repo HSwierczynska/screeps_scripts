a48fc30f48bb2486acc76adc1b58a4d63a78645b
Hswierczynska <haniasa96@gmail.com>
2017-08-07 02:26:14 +0200

    Started rewriting actions

--- creeps.actions.js
+++ creeps.actions.js
@@ -1,63 +1,39 @@
+//Actions for everyone
+
 function searchingForSources(creep){
-    var source = creep.pos.findClosestByRange(FIND_SOURCES);
-    if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-      }
- };
-
-
-
-function specifiedSource(creep, posX, posY){
-    var source = creep.room.lookForAt(LOOK_SOURCES, posX, posY);
-    if(creep.harvest(source[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(source[0]);
+    var source = creep.pos.findClosestByPath(FIND_SOURCES)  ;
+    if(creep.harvest(source) == ERR_NOT_IN_RANGE){
+        creep.moveTp(source,
+                     {visualizePathStyle:
+                      {stroke: '#FFB300'}});
     }
 };
 
 
 
 
-function dumpEnergy(creep){
-    var containerTarget = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: structure => {
-            return structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity
-        }
-    });
-    if(creep.transfer(containerTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-        creep.moveTo(containerTarget);
+function harvestingFromSpecifiedPlace(creep, posX, posY){
+    var source = creep.room.lookForAt(LOOK_SOURCES, posX, posY);
+    if(creep.harvest(source[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source[0]);
     }
-};    
 
-
-
-
-
-function collectDroppedEnergy(creep){
-        var droppedEnergy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-        if(droppedEnergy){
-            if(creep.pickup(droppedEnergy) == ERR_NOT_IN_RANGE){
-                creep.moveTo(droppedEnergy,
-                             {visualizePathStyle: {stroke: '#AD1457'}});
-            }
-        }
-
-
-    };
-
+};
 
 
 
 function withdrawFromEnergyStructure(creep, structureType){
     var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: structure => {
-            return structure.structureType == structureType && structure.store[RESOURCE_ENERGY] != 0 && ((structure.store[RESOURCE_ENERGY] < structure.storeCapacity) || (structure.store[RESOURCE_ENERGY] = structure.storeCapacity))
+            return structure.structureType == structureType &&
+                structure.store[RESOURCE_ENERGY] > 0
         }
     });
 
     if (target && creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(target, {
             visualizePathStyle: {
-                stroke: '#ffff00'
+                stroke: '#16a085'
             }
         });
     }
@@ -70,52 +46,75 @@ function withdrawFromEnergyStructure(creep, structureType){
 };
 
 
-function withdrawFromContainer(creep){
-    return withdrawFromEnergyStructure(creep, STRUCTURE_CONTAINER);
-};
 
-
-
+//Transfering to spawn, extensions, containers, storage and towers
 
 function transferingEnergy(creep, structureType){
     var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: (structure) => {
-            return(structure.structureType == structureType) && structure.energy < structure.energyCapacity
+            if (structure.store) {
+                return (structure.structureType == structureType) && structure.store < structure.storeCapacity
+            } else {
+                return (structure.structureType == structureType) && structure.energy < structure.energyCapacity
+            }   
         }
     });
-    if(target){
-        if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-            creep.moveTo(target);
+
+    if(target && creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+        creep.moveTo(target, {
+            visualizePathStyle: {
+                stroke: '#d35400'
+            }
+        });
+    }
+
+};
+
+
+//Dump energy at container or storage
+function leavingEnergy(creep){
+
+    var unfilledContainers = creep.room.find(FIND_STRUCTURES, {
+        filter: struct => struct.structureType == STRUCTURE_CONTAINER && struct.store[RESOURCE_ENERGY] < struct.storeCapacity 
+    });
+    if (unfilledContainers.length > 0) {
+        let target = creep.pos.findClosestByPath(unfilledContainers);
+        if(target && creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+            creep.moveTo(target, {
+                visualizePathStyle: {
+                    stroke: '#c0392b'
+                }
+            });
+        }
+    } else {
+        if (creep.room.storage) {
+            
         }
     }
-};
-
-
-function transferingToSpawn(creep){
-    return (transferingEnergy(creep, STRUCTURE_SPAWN));
-};
-
-
-function transferingToExtension(creep){
-    return (transferingEnergy(creep, STRUCTURE_EXTENSION));
-};
-
-function transferingToTower(creep){
-    return (transferingEnergy(creep, STRUCTURE_TOWER));
-};
-
-
-
-
-module.exports= {
-    searchingForSources,
-    specifiedSource,
-    dumpEnergy,
-    collectDroppedEnergy,
-    withdrawFromEnergyStructure,
-    withdrawFromContainer,
-    transferingEnergy,
-    transferingToSpawn,
-    transferingToExtension,
-    transferingToTower
+    
 }
+
+
+
+//Prole actions
+
+
+//Builder actions
+
+
+//Repairman actions
+
+
+//Harvester actions
+
+
+
+//Finder actions
+
+
+
+//Courier actions
+
+
+
+
