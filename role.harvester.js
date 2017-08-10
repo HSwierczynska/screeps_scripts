b9a5a68179ea4a1e10015a431f3803fada9748ed
Hswierczynska <haniasa96@gmail.com>
2017-08-10 20:43:25 +0200

    Harvesting energy from specified place

--- role.harvester.js
+++ role.harvester.js
@@ -5,14 +5,9 @@ var roleHarvester = {
 
     run: function(creep){
         if(creep.carry.energy == 0){
-            var otherHarvester = _.filter(Game.creeps, (creep) => creep.memory.role == 'Harvester');
-            if(otherHarvester.length == 0){
-                actions.specifiedSource(creep, 22, 12);
-            }
-            else{
-                actions.specifiedSource(creep, 34, 15);
-            }
+            actions.harvestingFromSpecifiedPlace(creep, posX, posY);
         }
+
         else{
             creep.drop(RESOURCE_ENERGY);
         }
